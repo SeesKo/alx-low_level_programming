dfb26c33a30c962aa124728c706abb3423692fcf
Siskosee <seeskosee@seeskoseesko.com>
2023-09-07 08:01:57 -0700

    Tasks 3,4,5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - the starting point for the program's execution
+ * Return: 0 means successful execution
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - starts the program
+ * Return: 0 means successful
+ */
+
 int main(void)
 {
-	
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
-
 }
