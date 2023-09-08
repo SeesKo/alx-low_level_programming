fe824093a93f1b024f38dd70103f4dcec8ae5eb7
Siskosee <seeskosee@seeskoseesko.com>
2023-09-08 12:23:56 -0700

    Tasks 8,9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers of base 16.
+ *
+ * Return: 0 means success.
+ */
+
+int main(void)
+{
+	int digit;
+	char wrds;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar((digit % 10) + '0');
+	for (wrds = 'a'; wrds <= 'f'; wrds++)
+		putchar(wrds);
+	putchar('\n');
+
+	return (0);
+}
