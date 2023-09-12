438694ec7f456d8c3893bea9fee346df7d6673c3
Siskosee <seeskosee@seeskoseesko.com>
2023-09-12 16:46:10 -0700

    Task 6,7,8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ *
+ * @i: Function parameter
+ *
+ * Return: The value of the last digit.
+ */
+
+int print_last_digit(int i)
+{
+	int j;
+	j = i % 10;
+
+	if (i < 0)
+		j = -j;
+	_putchar(j + '0');
+	return (j);
+}
+
