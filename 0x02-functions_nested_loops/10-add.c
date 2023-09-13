711c4f66ba81681159d271de740f108302aba97d
Siskosee <seeskosee@seeskoseesko.com>
2023-09-12 18:18:59 -0700

    All-Update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result.
+ * @a: First parameter.
+ * @b: Second parameter.
+ * Return: a + b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
