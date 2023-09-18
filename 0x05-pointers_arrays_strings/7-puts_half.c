99ab40434562715268ea3b123fd485a12a7fdace
Siskosee <seeskosee@seeskoseesko.com>
2023-09-18 16:28:48 -0700

    Printing the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,15 @@
 void puts_half(char *str)
 {
 	int i;
+	int half;
 	int strLength = 0;
 
 	/* looping through characters in string */
 	for (i = 0; str[i] != '\0'; i++)
 		strLength++;
-	for (i = 5; i < (strLength / 2); i++)
-	{
-		/* Printing only even numbers */
-		if (i % 2 != 0)
-			_putchar(str[i]);
-	}
+	/* getting the second half of the string */
+	half = (strLength - 1) / 2;
+	for (i = half + 1; str[i] != '\0'; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
