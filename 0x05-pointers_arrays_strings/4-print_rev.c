450d540a11457bc858b44a7b3d836f46aa6a9f57
Siskosee <seeskosee@seeskoseesko.com>
2023-09-18 18:46:45 -0700

    Printing a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,8 +11,10 @@ void print_rev(char *s)
 	int i;
 	int strLength = 0;
 
+	/* looping through the string to find the length of s */
 	for (i = 0; s[i] != '\0'; i++)
 		strLength++;
+	/* looping from the last element of s to the first */
 	for (i = strLength - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
