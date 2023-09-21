0783451fe13e155d007f8a355148db958ea2a7de
Siskosee <seeskosee@seeskoseesko.com>
2023-09-21 11:12:25 -0700

    Reversing array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,5 +9,15 @@
 
 void reverse_array(int *a, int n)
 {
+	int i;
+	int temporary;
 
+	for (i = 0; i < n / 2; i++)
+	{
+		/* Using variable temporary to swap 1st */
+		/* half values with 2nd half ones */
+		temporary = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temporary;
+	}
 }
