f21a30419a35d2d211140ccf4c8c6e471b812bd0
Siskosee <seeskosee@seeskoseesko.com>
2023-09-21 07:22:56 -0700

    Copying a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,5 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
+	/* Looping through src */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (i = 0; i < n && dest[i] == '\0'; i++)
+		;
+	return (dest);
 }
