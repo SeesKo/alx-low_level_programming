aac3781332e4bf9b4ae2b2d888aff65f94604095
Siskosee <seeskosee@seeskoseesko.com>
2023-09-21 12:36:03 -0700

    Encoding into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,11 +2,23 @@
 
 /**
  * leet - Encodes a string into 1337.
- *
+ * @str: Pointer to the string.
  * Return: Pointer to the encoded string.
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
+	int a, b;
+	char *ptr1 = "aAeEoOtTlL";
+	char *ptr2 = "4433007711";
 
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (str[a] == ptr1[b])
+				str[a] = ptr2[b];
+		}
+	}
+	return (str);
 }
