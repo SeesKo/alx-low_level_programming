e7a5641b43f2945c06444c9800ee9cab658cd0fc
Siskosee <seeskosee@seeskoseesko.com>
2023-09-21 12:09:07 -0700

    Capitalizing all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,11 +2,28 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- *
+ * @str: Pointer to the string.
  * Return: Pointer to capitalized string.
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	int i, k;
+	char separator[] = {' ', '\t', '\n', ',', ';', '.', '!',
+				'?', '"', '(', ')', '{', '}'};
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		for (k = 0; k < 13; k++)
+		{
+			if (str[i] == separator[k])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
 }
