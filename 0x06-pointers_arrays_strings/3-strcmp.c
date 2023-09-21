76f3e2b7678429cedd86deaed54b3ddc673ea699
Siskosee <seeskosee@seeskoseesko.com>
2023-09-21 08:48:12 -0700

    Comparing 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int i;
 
+	/* Looping through both s1 & s2 */
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1 != s2)
+		{
+			if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
