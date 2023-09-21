9d827860e65275205a702f23391b2cc1ad802206
Siskosee <seeskosee@seeskoseesko.com>
2023-09-21 06:33:05 -0700

    Concatenating 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,10 +13,13 @@ char *_strcat(char *dest, char *src)
 	int firstStr = 0;
 	int secondStr = 0;
 
+	/* Getting the length of the 1st string */
 	for (i = 0; dest[i] != '\0'; i++)
 		firstStr++;
+	/* Getting the length of the 2nd string */
 	for (i = 0; src[i] != '\0'; i++)
 		secondStr++;
+	/* Concatenation */
 	for (i = 0; i <= secondStr; i++)
 		dest[firstStr + i] = src[i];
 	return (dest);
