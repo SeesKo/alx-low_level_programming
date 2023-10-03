69af10216efe9c3d6f108fbdb0ee2fa7961c6cbb
Siskosee <seeskosee@seeskoseesko.com>
2023-10-03 03:41:38 -0700

    Duplicating a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,23 +10,33 @@
 
 char *_strdup(char *str)
 {
+	/* Declaring pointer to hold the duplicated string */
 	char *strcpy;
 	int i;
 	int len = 0;
 
+	/* Check if input string is null */
 	if (str == NULL)
 		return (NULL);
 
+	/* Calculating length of input string */
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
 
+	/* Memory allocation for duplicated string, */
+	/* + 1 space for the null terminator */
 	strcpy = (char *)malloc((len + 1) * sizeof(char));
 
+	/* Check if memory allocation was successful */
 	if (strcpy == NULL)
 		return (NULL);
 
+	/* Copying characters from given string into duplicated string */
 	for (i = 0; str[i] != '\0'; i++)
 		strcpy[i] = str[i];
+
+	/* Adding null-terminator to duplicated string */
+	strcpy[len] = '\0';
 
 	return (strcpy);
 }
