fbc58ffda9e264de94974e4402cf169dd8986d7e
Siskosee <seeskosee@seeskoseesko.com>
2023-10-03 03:51:55 -0700

    Creating character array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,17 +11,25 @@
 
 char *create_array(unsigned int size, char c)
 {
+	/* Declaring a pointer to hold created character array */
+	/* and declaring a loop counter */
 	char *arr;
 	unsigned int i;
 
+	/* Checking if size is 0 */
 	if (size == 0)
 		return (NULL);
 
+	/* Allocating memory for the character array */
+	/* using the specified size */
 	arr = (char *)malloc(size * sizeof(char));
 
+	/* Check if memory allocation was successful */
 	if (arr == NULL)
 		return (NULL);
 
+	/* Initialize each element of the character array */
+	/* with the specified char */
 	for (i = 0; i < size; ++i)
 		arr[i] = c;
 
