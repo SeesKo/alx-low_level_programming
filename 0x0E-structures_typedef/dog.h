607612ebd2656d573bee9d146d65c0276da7ce8f
Siskosee <seeskosee@seeskoseesko.com>
2023-10-09 15:48:55 -0700

    Defining a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog's details.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ *
+ * Description: This structure stores a dog's personal information.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
