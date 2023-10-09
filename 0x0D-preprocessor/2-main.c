2984101bc692ee04202bd642db5d38232034ce0c
Siskosee <seeskosee@seeskoseesko.com>
2023-10-09 08:11:35 -0700

    Printing name of source code file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the file it was compiled from.
+ *
+ * Return: O means success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
