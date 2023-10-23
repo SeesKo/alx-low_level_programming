032404e47c7a6ba3c94d138a177ab628ce784f84
Siskosee <seeskosee@seeskoseesko.com>
2023-10-23 12:35:30 -0700

    Sum of all data in list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - Returns the sum of all the data (n) in a listint_t
+ * sum_listint - Returns sum of all the data (n) in a listint_t
  * linked list.
  * @head: Pointer to the head of the linked list.
  * Return: Sum of all data in linked list, 0 if list is empty.
