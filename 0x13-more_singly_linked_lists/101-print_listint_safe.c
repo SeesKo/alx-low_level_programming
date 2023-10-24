f208198e2468be66d5ec15d13247e112529b6eec
Siskosee <seeskosee@seeskoseesko.com>
2023-10-24 04:05:58 -0700

    Printing list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the head of the linked list.
+ * Return: Number of nodes in the list.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t node_count = 0;
+
+	if (head == NULL)
+		exit(98);
+
+	while (head)
+	{
+		/* Printing address and value of current node */
+		printf("[%p] %d\n", (void *)head, head->n);
+		node_count++;
+
+		if (head <= head->next)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			exit(98);
+		}
+
+		head = head->next; /* Move to next node in the list */
+	}
+
+	return (node_count);
+}
