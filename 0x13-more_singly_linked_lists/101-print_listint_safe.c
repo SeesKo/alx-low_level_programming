9460d0719b255fc0bc7f97cf1cca9b6ab16eb380
Siskosee <seeskosee@seeskoseesko.com>
2023-10-24 17:55:30 -0700

    Printing list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,34 +3,45 @@
 /**
  * print_listint_safe - Prints a listint_t linked list.
  * @head: Pointer to the head of the linked list.
- *
  * Return: Number of nodes in the list, or -1 if an error occurs.
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t node_count = 0;
+	const listint_t *nodes[2048];
+	const listint_t *current = head;
+	size_t i;
+	int loop_detected = 0;
 
-	if (head == NULL)
-		return (0);
-
-	while (head)
+	for (i = 0; i < 2048; i++)
 	{
-		/* Print the address and value of the current node */
-		printf("[%p] %d\n", (void *)head, head->n);
-		node_count++;
-
-		/* Check for a potential loop in the linked list */
-		if (head <= (const listint_t *)head->next)
-		{
-			if (head->next != NULL)
-			{
-				printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			}
-			return (-1); /* Indicate an error condition */
-		}
-
-		head = head->next; /* Move to the next node in the list */
+		nodes[i] = NULL;
 	}
 
-	return (node_count); /* Return the number of nodes */
+	while (current)
+	{
+		for (i = 0; i < node_count; i++)
+		{
+			if (nodes[i] == current)
+			{
+				loop_detected = 1;
+				break;
+			}
+		}
+
+		if (loop_detected)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+
+		printf("[%p] %d\n", (void *)current, current->n);
+		nodes[node_count] = current;
+		node_count++;
+
+		current = current->next;
+	}
+
+	return (node_count);
 }
