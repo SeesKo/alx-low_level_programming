2a4e5333f1ded1b38dfe9011b4e04925a87b9a2c
Siskosee <seeskosee@seeskoseesko.com>
2023-10-26 09:34:17 -0700

    Value of a bit at given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
  * @index: Index of the bit to retrieve, starting from 0.
  *
  * Return: Value of the bit at a specific index, -1 if
- * error occurs
+ * error occurs.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
