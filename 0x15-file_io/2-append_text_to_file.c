d07e9d0eb710de5f91c4a181446245be97d98f64
Siskosee <seeskosee@seeskoseesko.com>
2023-10-30 11:26:55 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fptr == NULL)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 		fputs(text_content, fptr);
 
 	fclose(fptr);
