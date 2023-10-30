f4ab9783486da1f0c0e3475dbb60cb1c665409a1
Siskosee <seeskosee@seeskoseesko.com>
2023-10-30 08:45:51 -0700

    read text and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,3 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include "main.h"
 
 /**
@@ -12,44 +9,45 @@
  * Return: Number of letters it can read or print,
  * 0 on failure or error.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
+	int descriptor;
 	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	char *data_buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
+	descriptor = open(filename, O_RDONLY);
+	if (descriptor == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	data_buffer = malloc(sizeof(char) * letters);
+	if (data_buffer == NULL)
 	{
-		close(file_descriptor);
+		close(descriptor);
 		return (0);
 	}
 
-	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_read = read(descriptor, data_buffer, letters);
 	if (bytes_read == -1)
 	{
-		free(buffer);
-		close(file_descriptor);
+		free(data_buffer);
+		close(descriptor);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, data_buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		free(buffer);
-		close(file_descriptor);
+		free(data_buffer);
+		close(descriptor);
 		return (0);
 	}
 
-	free(buffer);
-	close(file_descriptor);
+	free(data_buffer);
+	close(descriptor);
 
 	return (bytes_written);
 }
