8fbeee957bccd823b07a6d6146e332a2e0c73ef2
Siskosee <seeskosee@seeskoseesko.com>
2023-10-30 06:47:17 -0700

    file-creating function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with specific permissions and writes content.
+ * @filename: The Name of file to create.
+ * @text_content: Text string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	/* Declare variables */
+	FILE *fptr;
+	size_t txtlen = 0;
+	size_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	/* Calculate the length of text_content */
+	while (text_content[txtlen] != '\0')
+		txtlen++;
+
+	/* Open file for writing */
+	fptr = fopen(filename, "w");
+	if (fptr == NULL)
+		return (-1); /* File creation failed */
+
+	/* Write text_content to the file */
+	bytes_written = fwrite(text_content, 1, txtlen, fptr);
+	fclose(fptr);
+
+	if (bytes_written == txtlen)
+		return (1); /* Success */
+
+	/* Set file permissions to -rw------- */
+	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
+		return (-1); /* chmod failed */
+
+	return (1); /* Success */
+}
