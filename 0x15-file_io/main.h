53021f034a137179a209d193e846946fa6ebeb16
Siskosee <seeskosee@seeskoseesko.com>
2023-10-30 12:56:52 -0700

    main header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -13,5 +15,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+int open_source_file(const char *filename);
+int open_destination_file(const char *filename);
+int copy_file(int source_fd, int destination_fd);
+int set_permissions(const char *filename);
 
 #endif /* MAIN_H */
