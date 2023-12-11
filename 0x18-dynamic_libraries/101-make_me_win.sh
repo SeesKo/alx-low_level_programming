371c837613db69c539c2d0140ef381fc3f080952
Siskosee <seeskosee@seeskoseesko.com>
2023-12-11 15:33:27 -0800

    jackpot winner

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q -O /tmp/libjackpot.so https://github.com/SeesKo/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libjackpot.so
-export LD_PRELOAD=/tmp/libjackpot.so ./gm 9 8 10 24 75 9
+export LD_PRELOAD=/tmp/libjackpot.so
