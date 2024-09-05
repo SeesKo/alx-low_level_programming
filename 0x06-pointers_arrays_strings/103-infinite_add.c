19f3a96e7cbe42001cde183112bee8693eb1971c
SeesKo <seeskosee@seeskome.com>
2024-09-05 06:36:15 -0700

    add two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,30 +2,22 @@
 #include <string.h>
 
 /**
- * infinite_add - Adds two numbers represented as strings.
+ * add_digits - Adds digits from two strings and handles carry.
  * @n1: The first number as a string.
  * @n2: The second number as a string.
  * @r: The buffer to store the result.
  * @size_r: The size of the buffer.
+ * @i: Current index in the first number.
+ * @j: Current index in the second number.
+ * @carry: Carry value to add.
+ * @k: Index for storing the result.
  *
- * Return: A pointer to the result string, or 0 if the result cannot
- * fit in the buffer.
+ * Return: 1 if successful, 0 if buffer overflow.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int add_digits(char *n1, char *n2, char *r, int size_r,
+		int i, int j, int carry, int *k)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0, sum;
-	int i, j, k;
-
-	if (size_r <= (len1 > len2 ? len1 : len2) + 1)
-	{
-		return (0);
-	}
-
-	i = len1 - 1;
-	j = len2 - 1;
-	k = 0;
+	int sum;
 
 	while (i >= 0 || j >= 0 || carry)
 	{
@@ -44,19 +36,46 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		}
 
 		carry = sum / 10;
-		if (k >= size_r - 1)
+		if (*k >= size_r - 1)
 		{
 			return (0);
 		}
-		r[k++] = (sum % 10) + '0';
+		r[(*k)++] = (sum % 10) + '0';
 	}
 
-	r[k] = '\0';
+	return (1);
+}
 
-	if (k >= size_r)
+/**
+ * infinite_add - Adds two numbers represented as strings.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
+ *
+ * Return: A pointer to the result string, or 0 if the result cannot
+ * fit in the buffer.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int i = len1 - 1;
+	int j = len2 - 1;
+	int k = 0;
+	int carry = 0;
+
+	if (size_r <= (len1 > len2 ? len1 : len2) + 1)
 	{
 		return (0);
 	}
+
+	if (!add_digits(n1, n2, r, size_r, i, j, carry, &k))
+	{
+		return (0);
+	}
+
+	r[k] = '\0';
 
 	for (i = 0; i < k / 2; i++)
 	{
