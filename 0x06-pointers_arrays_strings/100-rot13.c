356ae87c064c648fd6bb4ac9c184f38106336de9
SeesKo <seeskosee@seeskome.com>
2024-09-05 04:58:30 -0700

    encode a string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using the ROT13 cipher.
+ * @s: The string to encode.
+ *
+ * Return: The encoded string.
+ */
+char *rot13(char *s)
+{
+	char *p = s;
+
+	while (*p)
+	{
+		if ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z'))
+		{
+			if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+			{
+				*p += 13;
+			}
+			else
+			{
+				*p -= 13;
+			}
+		}
+		p++;
+	}
+
+	return s;
+}
