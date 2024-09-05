fc3a73ecd80acd66caa6d14674a16816f3c50554
SeesKo <seeskosee@seeskome.com>
2024-09-05 05:22:40 -0700

    print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,23 +6,14 @@
  */
 void print_number(int n)
 {
-	int divisor = 1;
-	int num = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -n;
+		n = -n;
 	}
 
-	while (num / divisor >= 10)
-	{
-		divisor *= 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
 
-	while (divisor > 0)
-	{
-		_putchar((num / divisor) % 10 + '0');
-		divisor /= 10;
-	}
+	_putchar((n % 10) + '0');
 }
